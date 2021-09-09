be5b4add91b22c2a1e5f4cc031834f1de5f55241
jumail j <jumailjuraige@gmail.com>
2021-09-09 23:06:08 +0530

    added project Generation File(PreMake)

---
+++ premake5.lua
@@ -0,0 +1,75 @@
+--premake5.lua
+
+workspace "Engine"
+	architecture"x64"
+	configurations {"Debug", "Development", "Ship"}
+
+	outputdir= "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Engine"
+	location "Engine"
+	kind "SharedLib" --dll
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}") 
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files {"%{prj.name}/src/**.h", "%{prj.name}/src/**.cpp"}
+	includedirs {"%{prj.name}/vendor/spdlog/include"}
+
+	filter	"system:windows"
+		cppdialect "c++17"
+		staticruntime"On"
+		systemversion "latest"
+
+	defines {"ENGINE_PLATFORM_WINDOWS", "ENGINE_BUILD_DLL"}
+
+	--copy engine.dll -> sandbox
+	postbuildcommands ("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+	
+	filter "configurations:Debug"
+		defines "ENGINE_DEBUG"
+		symbols "On"
+
+	filter "configurations:Development"
+		defines "ENGINE_DEVELOPMENT"
+		optimize "On"
+
+	filter "configurations:Ship"
+		defines "ENGINE_SHIP"
+		optimize "On"
+
+
+project "Sandbox"
+	location "Sandbox"
+	kind "consoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files {"%{prj.name}/src/**.h", "%{prj.name}/src/**.cpp"}
+
+	includedirs {"Engine/vendor/spdlog/include", "Engine/src"}
+
+	links { "Engine"}
+
+	filter	"system:windows"
+		cppdialect "c++17"
+		staticruntime"On"
+		systemversion "latest"
+
+	defines {"ENGINE_PLATFORM_WINDOWS"}
+
+
+	filter "configurations:Debug"
+		defines "ENGINE_DEBUG"
+		symbols "On"
+
+	filter "configurations:Development"
+		defines "ENGINE_DEVELOPMENT"
+		optimize "On"
+
+	filter "configurations:Ship"
+		defines "ENGINE_SHIP"
+		optimize "On"
