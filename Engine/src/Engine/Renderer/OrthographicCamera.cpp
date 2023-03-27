d9603771783e2d61091824cf8e0f4854917104e7
jumailj <jumailj.work@gmail.com>
2023-03-27 19:03:48 +0530

    new camera controller for engine

--- Engine/src/Engine/Renderer/OrthographicCamera.cpp
+++ Engine/src/Engine/Renderer/OrthographicCamera.cpp
@@ -11,9 +11,17 @@ namespace Engine {
 		m_ViewProjectionMatrix = m_ProjectionMatrix * m_ViewMatrix;
 
 	}
+
+	void OrthographicCamera::SetProjection(float left, float right, float bottom, float top)
+	{
+		m_ProjectionMatrix = glm::ortho(left, right, bottom, top, -1.0f, 1.0f);
+		m_ViewProjectionMatrix = m_ProjectionMatrix * m_ViewMatrix;
+	}
+
+
 	void OrthographicCamera::RecalculateViewMatrix()
 	{
-		glm::mat4 transfrom = glm::translate(glm::mat4(1.0f), m_Position) * 
+		glm::mat4 transfrom = glm::translate(glm::mat4(1.0f), m_Position) *
 			glm::rotate(glm::mat4(1.0), glm::radians(m_Rotation), glm::vec3(0, 0, 1));
 
 		m_ViewMatrix = glm::inverse(transfrom);
