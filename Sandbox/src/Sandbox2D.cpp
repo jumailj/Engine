add7d3dabf5369b3385fe61ebb9fad185d8ff393
jumailj <jumailj.work@gmail.com>
2023-04-13 15:12:47 +0530

    rotational batch texture

--- Sandbox/src/Sandbox2D.cpp
+++ Sandbox/src/Sandbox2D.cpp
@@ -7,13 +7,14 @@
 Sandbox2D::Sandbox2D()
 	: Layer("Sandbox2D"), m_CameraController(1280.0f / 720.0f, true)
 {
+
 }
 
 void Sandbox2D::OnAttach()
 {
 	ENGINE_PROFILE_FUNCTION();
 
-	m_CheckerboardTexture = Engine::Texture2D::Create("assets/textures/Checkerboard.png");
+	m_CheckerboardTexture = Engine::Texture2D::Create("assets/textures/Checkerboard1.png");
 }
 
 void Sandbox2D::OnDetach()
@@ -36,12 +37,19 @@ void Sandbox2D::OnUpdate(Engine::Timestep ts)
 	}
 
 	{
+
+		static float roation = 0.0f;
+		roation += ts * 20.0f;
+
+
 		ENGINE_PROFILE_SCOPE("Renderer Draw");
 		Engine::Renderer2D::BeginScene(m_CameraController.GetCamera());
-		Engine::Renderer2D::DrawRotatedQuad({ -1.0f, 0.0f }, { 0.8f, 0.8f }, glm::radians(-45.0f), { 0.8f, 0.2f, 0.3f, 1.0f });
-		Engine::Renderer2D::DrawQuad({ -1.0f, 0.0f }, { 0.8f, 0.8f }, { 0.8f, 0.2f, 0.3f, 1.0f });
+	// 	Engine::Renderer2D::DrawRotatedQuad({ -1.0f, 0.0f }, { 0.8f, 0.8f }, glm::radians(-45.0f), { 0.8f, 0.2f, 0.3f, 1.0f });
+
 		Engine::Renderer2D::DrawQuad({ 0.5f, -0.5f }, { 0.5f, 0.75f }, { 0.2f, 0.3f, 0.8f, 1.0f });
-		Engine::Renderer2D::DrawQuad({ 0.0f, 0.0f, -0.1f }, { 10.0f, 10.0f }, m_CheckerboardTexture, 3.0f);
+		Engine::Renderer2D::DrawQuad({ 0.0f, 0.0f, -0.1f }, { 10.0f, 10.0f }, m_CheckerboardTexture, 10.0f);
+		Engine::Renderer2D::DrawRotatedQuad({ -2.0f, 0.0f, 0.0f }, { 1.0f, 1.0f }, roation, m_CheckerboardTexture, 20.0f);
+
 		Engine::Renderer2D::EndScene();
 	}
 }
